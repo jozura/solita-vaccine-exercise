3863358b4ad561cd94ca15bc2ba9cb9bb596a22d
jozura <johannes.haapakoski@outlook.com>
2021-08-15 18:25:47 +0300

    fix init

--- backend/init.js
+++ backend/init.js
@@ -12,6 +12,43 @@ const dbConnection = mysql.createConnection({
   database : process.env.DB_NAME 
 });
 
+const createVaccineOrderTable = () => {
+  return new Promise((resolve, reject) => {
+    dbConnection.query(`CREATE TABLE VaccineOrder (
+  Id CHAR(36) NOT NULL UNIQUE,
+  HealthCareDistrict ENUM('HYKS', 'KYS', 'OYS', 'TAYS', 'TYKS') NOT NULL,
+  OrderNumber INT NOT NULL UNIQUE,
+  ResponsiblePerson VARCHAR(255) NOT NULL,
+  Arrived DATETIME NOT NULL,
+  Vaccine ENUM('Zerpfy', 'Antiqua', 'SolarBuddhica') NOT NULL,
+  Injections INT NOT NULL, 
+
+  PRIMARY KEY (Id)
+);
+`, function (error, results, fields) {
+      if (error) reject(error)
+      resolve()
+    });
+  });
+}
+
+const createVaccinationTable = () => {
+  return new Promise((resolve, reject) => {
+    dbConnection.query(`CREATE TABLE Vaccination (
+  Id CHAR(36) NOT NULL UNIQUE,
+  SourceBottle CHAR(36) NOT NULL,
+  Gender ENUM('male', 'female', 'nonbinary') NOT NULL,
+  VaccinationDate DATETIME NOT NULL,
+
+  PRIMARY KEY (Id),
+  FOREIGN KEY (SourceBottle) REFERENCES VaccineOrder(Id)
+);`, function (error, results, fields) {
+      if (error) reject(error)
+      resolve()
+    });
+  });
+}
+
 const orderToDb = async (values) => {
   values = values.map(val => {
     let { id, healthCareDistrict, orderNumber, responsiblePerson, arrived, vaccine, injections } = val;
@@ -62,6 +99,8 @@ const processLineByLine = async (fn, toDb) => {
   return toDb(values);
 }
 
+await createVaccineOrderTable();
+await createVaccinationTable();
 await processLineByLine('./resources/Antiqua.source', orderToDb);
 await processLineByLine('./resources/SolarBuddhica.source', orderToDb);
 await processLineByLine('./resources/Zerpfy.source', orderToDb);
